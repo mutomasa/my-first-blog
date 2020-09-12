f290ee4e3f2907a6cd6ea46660128336f7ee6e29
mutomasa <mutomasa118@gmail.com>
2020-09-12 21:56:17 +0900

    シークレットキーを隠す

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +25,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 
-SECRET_KEY = '*2t=sp@&wjk8%1igg6p(q1fav_2y!$_jzdk@znp==(u&3k3(v6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
