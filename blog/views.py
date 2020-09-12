8bb335c1507b548e054632bf064a69ce875d3f91
mutomasa <mutomasa118@gmail.com>
2020-09-12 21:29:25 +0900

    フォームを作成

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render,get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
+from django.shortcuts import redirect #ページを推移させる
+
 
 # Create your views here.
 def post_list(request):
@@ -12,3 +15,18 @@ def post_list(request):
 def post_detail(request,pk):
     post = get_object_or_404(Post,pk=pk)
     return render(request,'blog/post_detail.html',{'post':post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST) #POSTに投稿データが入っている
+        #フォームの内容をチェック
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            #詳細ページへリダイレクト
+            return redirect('post_detail',pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request,'blog/post_edit.html',{'form':form})
